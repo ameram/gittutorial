6139110505530258edc88b57bdb587af8a8c7673
Amir M. Ramzani <playergood0@live.com>
2018-09-18 00:57:02 +0430

    Update lazzyPython.py
    گفتم کمی کامنت بنویسم و خوانا تر کنم

--- python/lazzyPython.py
+++ python/lazzyPython.py
@@ -1,16 +1,24 @@
 #!/usr/bin/python3
 
+# specifying the main method, perfect python code
+
 
 def main():
-    a = 1.1
-    b = 2.2
+    
+    # defining the first variable named "first_number" and initializing it to 1.1
+    first_number = 1.1
 
-    print('a =', a)
-    print('b =', b)
-    print('and, magic is happen')
-    print('a + b =', a+b)
-    print('what the heck is going on here?')
-    print('do you have any idea?')
+    # defining the second variable named "second_number" and initializing it to 2.2
+    second_number = 2.2
 
+    print('First Number =', first_number)  # First number 1.1
+    print('Second number =', second_number)
+    print('And, magic is happen')
+    print('Sum of First and second number =', first_number + second_number)
+    print('What the heck is going on here?')
+    print('Do you have any idea?')
+
+
+# This is the main chunk of code we call and execute main method here in main namespace
 if __name__ == '__main__':
     main()
